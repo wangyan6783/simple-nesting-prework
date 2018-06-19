7f4187c7ad6a353622b3a6205f4b30fe2b06abcc
Yan Wang <wangyan6783@gmail.com>
2018-06-19 12:50:54 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,9 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = 
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP, C"]
 
 :yukihiro_matsumoto => {
   :known_for => "Ruby",
