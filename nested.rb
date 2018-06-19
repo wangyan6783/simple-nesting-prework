79fae235723532a0fb0d1d61049d1b652ef80f53
Yan Wang <wangyan6783@gmail.com>
2018-06-19 12:51:44 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,9 @@ def adding_matz
 
 programmer_hash[:yukihiro_matsumoto] = {}
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP, C"]
-
+programmer_hash[:yukihiro_matsumoto][:languages] = []
+programmer_hash[:yukihiro_matsumoto][:languages] << "LISP"
+programmer_hash[:yukihiro_matsumoto][:languages] << "C"
 :yukihiro_matsumoto => {
   :known_for => "Ruby",
     :languages => ["LISP, C"]
