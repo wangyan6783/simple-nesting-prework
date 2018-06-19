a40867e94f22bcab157aefb05964ebd2e467e903
Yan Wang <wangyan6783@gmail.com>
2018-06-19 13:05:27 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ programmer_hash[:yukihiro_matsumoto][:languages] = []
 programmer_hash[:yukihiro_matsumoto][:languages] << "LISP"
 programmer_hash[:yukihiro_matsumoto][:languages] << "C"
 
-programmer_hash[:yukihiro_matsumoto]
+programmer_hash
 
 end
 
