ad59bb0671eb53782cb24d4e980d484edade5ebb
Yan Wang <wangyan6783@gmail.com>
2018-06-19 12:45:37 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,38 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
+     
   programmer_hash[:grace_hopper]
 end
 
-
-  describe "#dennis_ritchies_language" do 
-    it "operates on the programmer_hash and returns the value of Dennis Ritchie's language as a string" do 
-      expect(dennis_ritchies_language).to eq("C")
-    end
-  end
-
-  describe "#adding_matz" do 
-    it "operates on the programmer_hash and adds a key/value pair to the top level of the hash, returning the newly-added-to hash" do 
-      expect(adding_matz.keys).to include(:yukihiro_matsumoto)
-    end
-  end
-
-  describe "#changing_alan" do 
-    it "operates on the programmer_hash and changes what Alan Kay is known for, returning the newly-changed hash" do 
-      expect(changing_alan[:alan_kay][:known_for]).to eq("GUI")
-    end
-  end
-
-  describe "#adding_to_dennis" do 
-    it "operates on the programmer_hash and adds 'Assembly' to Dennis Ritchie's languages, returning the newly-added-to-hash" do 
-      expect(adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
-    end
-  end
-end
---------------------
-
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -84,6 +57,33 @@ def dennis_ritchies_language
         }
      }
 end
+
+ describe "#dennis_ritchies_language" do 
+    it "operates on the programmer_hash and returns the value of Dennis Ritchie's language as a string" do 
+      expect(dennis_ritchies_language).to eq("C")
+    end
+  end
+
+  describe "#adding_matz" do 
+    it "operates on the programmer_hash and adds a key/value pair to the top level of the hash, returning the newly-added-to hash" do 
+      expect(adding_matz.keys).to include(:yukihiro_matsumoto)
+    end
+  end
+
+  describe "#changing_alan" do 
+    it "operates on the programmer_hash and changes what Alan Kay is known for, returning the newly-changed hash" do 
+      expect(changing_alan[:alan_kay][:known_for]).to eq("GUI")
+    end
+  end
+
+  describe "#adding_to_dennis" do 
+    it "operates on the programmer_hash and adds 'Assembly' to Dennis Ritchie's languages, returning the newly-added-to-hash" do 
+      expect(adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
+    end
+  end
+end
+--------------------
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
