ed3aa3eba0620e9406961072d20656dc2f17cc76
Yan Wang <wangyan6783@gmail.com>
2018-06-19 12:45:13 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -19,12 +19,6 @@ def hopper
 end
 
 
-  describe "#alan_kay_is_known_for" do
-    it "operates on the programmer_hash and returns the value of what Alan Kay is known for" do 
-      expect(alan_kay_is_known_for).to eq("Object Orientation")
-    end 
-  end
-
   describe "#dennis_ritchies_language" do 
     it "operates on the programmer_hash and returns the value of Dennis Ritchie's language as a string" do 
       expect(dennis_ritchies_language).to eq("C")
